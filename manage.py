ed661a3da796d7712945ed20f05f7e720c9e719c
tarobeee <s194901w@gmail.com>
2022-11-14 22:21:20 +0900

    commit test

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# this is test for git commit from remote repositly
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -8,6 +10,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp1.settings')
     try:
         from django.core.management import execute_from_command_line
+        print("cloneしたremoteからcommitするテスト")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
